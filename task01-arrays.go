917a26cff448faa02336fbd1ca1a628a4df79bfc
Aidyn Omarov <aidyn_o_b@mail.ru>
2022-05-11 17:04:53 +0600

    finish

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,10 @@
 package homework
 
-func average(input [15]float32) (result float32) {
-	//Place your code here
+func Average(input [15]float32) (result float32) {
+	var total float32
+	for _, number := range input {
+		total = total + number
+	}
+	result = total / 15
 	return
 }
--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,10 @@
 package homework
 
-func reverse(input []int64) (result []int64) {
+func Reverse(input []int64) (result []int64) {
 	//Place your code here
+	var reverseSlice []int64
+	for i := len(input) - 1; i >= 0; i-- {
+		reverseSlice = append(reverseSlice, input[i])
+	}
 	return
 }
